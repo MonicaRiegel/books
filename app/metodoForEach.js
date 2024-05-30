82fe2ed2ab7d7990e15c6128fa7514862963b88e
Monica Riegel <monicariegel@hotmail.com>
2024-05-30 12:54:00 -0300

    Otimizando filtros

--- app/metodoForEach.js
+++ app/metodoForEach.js
@@ -1,6 +1,8 @@
 const elementoParaInserirLivros = document.getElementById('livros')
+const elenemtoComValorTotalDeLivrosDisponiveis = document.getElementById('valor_total_livros_disponiveis')
 
 function exibirOsLivrosNaTela(listaDeLivros) {
+  elenemtoComValorTotalDeLivrosDisponiveis.innerHTML = ''
     elementoParaInserirLivros.innerHTML = ''
     listaDeLivros.forEach(livro => {
         //let disponibilidade = verificarDisponibilidadeDoLivro(livro)
